15a2c93290bbdfccd64aaf76852f74f1b465f1c1
Saiteja Aerupula <144018477+SaitejaAerupula@users.noreply.github.com>
2025-07-18 05:28:28 +0530

    Add files via upload

---
+++ Day 2.js
@@ -0,0 +1,25 @@
+let a="Saiteja";
+let age=21;
+let isStudent=true;
+let score=95.5;
+console.log(typeof a); // string
+console.log(typeof age); // number 
+console.log(typeof isStudent); // boolean
+console.log(typeof score); // number
+
+let firstname = "Saiteja";
+let lastname = "Aerupula";
+let fullName = firstname + " " + lastname;
+console.log("Full Name: " + fullName);
+
+let isGraduated = false;
+let graduationYear = 2026;
+let graduationMessage = isGraduated ? "Congratulations on graduating!" : "Keep working towards your graduation!";
+console.log(graduationMessage + " Expected year: " + graduationYear);
+
+
+let x;
+console.log(x);
+
+let result=(5*3)*2-4/2;
+console.log(result);
