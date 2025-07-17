94fe4e9988b96f6afb6a4bf9aaaaad4bbda8d6ca
Saiteja Aerupula <144018477+SaitejaAerupula@users.noreply.github.com>
2025-07-17 14:49:45 +0530

    Add files via upload

---
+++ Day 1.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+
+var greeting = "learning javascript!";
+console.log(greeting);
+
+let number = 42;
+console.log("The answer to life, the universe, and everything is: " + number);
+
+const pi = 3.14159;
+console.log("The value of pi is approximately: " + pi);
+
+var a = 5;
+var b = 10;
+let sum = a + b;   
+console.log("The sum of " + a + " and " + b + " is: " + sum);
